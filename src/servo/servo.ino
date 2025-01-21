de54982262dbd4616e0597b709002c99b0913bf0
tang_jingfan <tangjingfan@gmail.com>
2025-01-21 14:02:10 +0800

    feat: add arduino control servo

---
+++ src/servo/servo.ino
@@ -0,0 +1,29 @@
+#include <Servo.h>
+
+// global variable
+int angle = 0;
+Servo ackermann_chassis;
+String command;
+
+void setup() {
+  ackermann_chassis.attach(10);
+  Serial.begin(115200);
+}
+
+void loop() {
+  while (Serial.available() > 0) {
+    char temp = char(Serial.read());
+    command += temp;
+  }
+
+  if (command.length() > 0) {
+    int begin = command.indexOf('<');
+    int end = command.indexOf('>') + 1;
+    if (begin != -1 && end != -1 && begin < end) {
+      sscanf(command.substring(begin, end).c_str(), "<SERVO:%d>", &angle);
+      ackermann_chassis.write(angle);
+    }
+  }
+  command = "";
+  delay(15);
+}
